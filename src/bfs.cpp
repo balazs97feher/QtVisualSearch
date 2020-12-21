764f5d4a9ad221856a7fbeef8df61e53c70a6079
FBalazs <balazs97feher@gmail.com>
2020-12-21 14:51:36 +0100

    Debug bfs: might still be erronous

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -2,13 +2,14 @@
 
 using namespace::std;
 
-BFS::BFS(shared_ptr<SearchGrid> grid) : PathFinder(grid), dirIndex(0)
+BFS::BFS(shared_ptr<SearchGrid> grid) : PathFinder(grid)
 {
     previousTile.resize(grid->rowCount);
     for(auto &row : previousTile) row.resize(grid->colCount);
 
     directions = grid->tiles[0][0]->getDirections();
     directionCount = directions.size();
+    dirIndex = directionCount;
 }
 
 
@@ -40,16 +41,19 @@ bool BFS::advance()
 
     while (!neighbor || (neighbor->type != TileType::Empty && neighbor->type != TileType::Destination))
     {
+        if(DEBUG_MSGS_ON) qDebug() << "[BFS] OUTER " << dirIndex << Qt::endl;
         if(dirIndex == directionCount)
         {
             if(tilesToCheck.empty()) return false;
+            if(DEBUG_MSGS_ON) qDebug() << "[BFS] poppin " << dirIndex << Qt::endl;
             currentTile = tilesToCheck.front();
             tilesToCheck.pop_front();
             dirIndex = 0;
         }
 
-        while((!neighbor || (neighbor->type != TileType::Empty && neighbor->type != TileType::Destination)) && dirIndex < 4)
+        while((!neighbor || (neighbor->type != TileType::Empty && neighbor->type != TileType::Destination)) && dirIndex < directionCount)
         {
+            if(DEBUG_MSGS_ON) qDebug() << "[BFS] dir iter " << dirIndex << Qt::endl;
             neighborCoords = grid->tiles[0][0]->getNeighborCoords(currentTile, PathFinder::directions[dirIndex]);
             neighbor = grid->at(neighborCoords);
             dirIndex++;
@@ -64,6 +68,7 @@ bool BFS::advance()
         return false;
     }
     neighbor->type = TileType::Visited;
+    if(DEBUG_MSGS_ON) qDebug() << "[BFS] push back: " << neighborCoords.rowNum << ' ' << neighborCoords.colNum << Qt::endl;
     tilesToCheck.push_back(neighborCoords);
 
     static uint visitId = 0;
